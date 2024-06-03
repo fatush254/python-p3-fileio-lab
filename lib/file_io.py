dfd33926346c18475bf9effbd90ce54169bb0b09
fatush254 <fatuma.galgalo@student.moringaschool.com>
2024-06-03 08:33:36 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", mode ='w') as write_file:
+        write_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", mode = 'a') as append_file:
+        append_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode = 'r') as read_file:
+        return read_file.read() 
+        pass
